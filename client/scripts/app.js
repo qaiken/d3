8692cb233c3f602d2449393b7f11ae33e947f094
MKS-students <mks.students@hackreactor.com>
2015-07-13 17:34:49 -0700

    finish stacking

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,11 +25,9 @@ var app;
     //Y-Axis is rent amount, linear scaling
     //X-Axis is time, ordinal scaling
 
-    var dates = data.map(function(item){
-      return item[0].x;
+    var dates = data[0].map(function(point){
+      return point.x;
     });
-
-    console.table(dates)
 
     var x = d3.scale.ordinal()
         .domain(dates)
@@ -39,35 +37,55 @@ var app;
         .domain([0, yStackMax])
         .range([height, 0]);
 
-    var color = d3.scale.linear()
-        .domain([0, numLayers - 1])
-        .range(["#aad", "#556"]);
+    // var color = d3.scale.linear()
+    //     .domain([0, numLayers - 1])
+    //     .range(["#aad", "#556", 'green']);
+
+     var color = d3.scale.category10();
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        //.tickSize(0)
-        //.tickPadding(1)
         .outerTickSize(0)
         .orient("bottom");
 
+    //Created SVG, set width/height
     var svg = d3.select('.firstGraph')
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var layer = svg.selectAll(".layer")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "layer")
+                .style("fill", function(d, i) { return color(i); });
+
+    var rect = layer.selectAll("rect")
+                .data(function(d) { return d; })
+                .enter().append("rect")
+                .attr("x", function(d) { return x(d.x); })
+                .attr("y", height)
+                .attr("width", x.rangeBand())
+                .attr("height", 0);
+
+        rect.transition()
+            .delay(function(d, i) { return i * 10; })
+            .attr("y", function(d) { return y(d.y0 + d.y); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+    //Draw x-axis on the SVG
     svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis)
     .selectAll("text")
-      //.attr("y", 0)
       .attr("x", 5)
       .attr("dy", "-.35em")
       .attr("transform", "rotate(90)")
       .style("text-anchor", "start");
-  };
 
+  };
 
 
 }());
